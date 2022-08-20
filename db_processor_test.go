f9f38337854b9cb096fe2e382f877bd3a28f1564
Denis Skripov <nizhikebinesi@gmail.com>
2022-08-21 01:43:54 +0600

    add tests for /api/load_file/ and /api/load_from_url/

--- db_processor_test.go
+++ db_processor_test.go
@@ -4,8 +4,12 @@ import (
 	"bytes"
 	"golang-developer-test-task/redclient"
 	"golang-developer-test-task/structs"
+	"io"
+	"mime/multipart"
 	"net/http"
 	"net/http/httptest"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/mailru/easyjson"
@@ -102,8 +106,9 @@ func TestHandleLoadFromURLBadRequest(t *testing.T) {
 	}
 }
 
-func TestHandleLoadFromURLBadRequestWrongURL(t *testing.T) {
+func TestHandleLoadFromURLGoodURLWithErrorDueAdding(t *testing.T) {
 	db, _ := redismock.NewClientMock()
+	// TODO: add data to mock before it
 	client := &redclient.RedisClient{*db}
 
 	logger, _ := zap.NewProduction()
@@ -113,7 +118,7 @@ func TestHandleLoadFromURLBadRequestWrongURL(t *testing.T) {
 
 	processor := DBProcessor{client: client, logger: logger}
 
-	urlObject := structs.URLObject{URL: ""}
+	urlObject := structs.URLObject{URL: "http://op.mos.ru/opendata/files/7704786030-TaxiParking/data-20200706T0000-structure-20200706T0000.json"}
 	bs, err := easyjson.Marshal(urlObject)
 	if err != nil {
 		t.Fatal(err)
@@ -125,8 +130,8 @@ func TestHandleLoadFromURLBadRequestWrongURL(t *testing.T) {
 	h := processor.CheckHandlerRequestMethod(processor.HandleLoadFromURL, "POST")
 	h(res, req)
 
-	if res.Code != http.StatusBadRequest {
-		t.Errorf("got status %d but wanted %d", res.Code, http.StatusBadRequest)
+	if res.Code != http.StatusOK {
+		t.Errorf("got status %d but wanted %d", res.Code, http.StatusOK)
 	}
 }
 
@@ -152,7 +157,7 @@ func TestHandleLoadFileBadRequest(t *testing.T) {
 	}
 }
 
-func TestHandleLoadFromURL(t *testing.T) {
+func TestHandleLoadFromURLWrongMethod(t *testing.T) {
 	db, _ := redismock.NewClientMock()
 	client := &redclient.RedisClient{*db}
 
@@ -173,7 +178,7 @@ func TestHandleLoadFromURL(t *testing.T) {
 	}
 }
 
-func TestHandleLoadFile(t *testing.T) {
+func TestHandleLoadFileWrongMethod(t *testing.T) {
 	db, _ := redismock.NewClientMock()
 	client := &redclient.RedisClient{*db}
 
@@ -191,5 +196,53 @@ func TestHandleLoadFile(t *testing.T) {
 
 	if res.Code != http.StatusInternalServerError {
 		t.Errorf("got status %d but wanted %d", res.Code, http.StatusInternalServerError)
+	}
+}
+
+func TestHandleLoadFile(t *testing.T) {
+	db, _ := redismock.NewClientMock()
+	// TODO: add data to mock before it
+	client := &redclient.RedisClient{*db}
+
+	logger, _ := zap.NewProduction()
+	defer func() {
+		_ = logger.Sync()
+	}()
+
+	processor := DBProcessor{client: client, logger: logger}
+
+	filePath := "test_data/data.json"
+	file, err := os.Open(filePath)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		err = file.Close()
+		if err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	part, _ := writer.CreateFormFile("uploadFile", filepath.Base(file.Name()))
+	_, err = io.Copy(part, file)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = writer.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req := httptest.NewRequest("POST", "/api/load_file", body)
+	req.Header.Add("Content-Type", writer.FormDataContentType())
+	res := httptest.NewRecorder()
+	// processor.HandleLoadFile(res, req)
+	h := processor.CheckHandlerRequestMethod(processor.HandleLoadFile, "POST")
+	h(res, req)
+
+	if res.Code != http.StatusOK {
+		t.Errorf("got status %d but wanted %d", res.Code, http.StatusOK)
 	}
 }
