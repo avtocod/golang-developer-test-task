28d92799918118281b52e4bd459e439d84ed94ff
Denis Skripov <nizhikebinesi@gmail.com>
2022-08-22 02:34:52 +0600

    remove wrong way of making transaction

--- infrastructure/redclient/utils.go
+++ infrastructure/redclient/utils.go
@@ -10,127 +10,38 @@ import (
 
 // AddValue add info to Redis storage
 func (r *RedisClient) AddValue(ctx context.Context, info structs.Info) (err error) {
-	defer func() {
-		if err != nil {
-			err = fmt.Errorf("in AddValue: %w", err)
-		}
-	}()
+	// TODO: add transactions - https://stackoverflow.com/questions/40999446/how-to-create-redis-transaction-in-go-using-go-redis-redis-package
 	bs, err := easyjson.Marshal(info)
 	if err != nil {
 		return err
 	}
 	err = r.Set(ctx, info.SystemObjectID, bs, 0).Err()
 	if err != nil {
-		e := r.Del(ctx, info.SystemObjectID).Err()
-		if e != nil {
-			return e
-		}
 		return err
 	}
-	// TODO: add rollout when Set/RPush fails
-	globalId := fmt.Sprintf("global_id:%d", info.GlobalID)
-	err = r.Set(ctx, globalId, info.SystemObjectID, 0).Err()
+	globalID := fmt.Sprintf("global_id:%d", info.GlobalID)
+	err = r.Set(ctx, globalID, info.SystemObjectID, 0).Err()
 	if err != nil {
-		e := r.Del(ctx, info.SystemObjectID).Err()
-		if e != nil {
-			return e
-		}
-		e = r.Del(ctx, globalId).Err()
-		if e != nil {
-			return e
-		}
 		return err
 	}
 	id := fmt.Sprintf("id:%d", info.ID)
 	err = r.Set(ctx, id, info.SystemObjectID, 0).Err()
 	if err != nil {
-		e := r.Del(ctx, info.SystemObjectID).Err()
-		if e != nil {
-			return e
-		}
-		e = r.Del(ctx, globalId).Err()
-		if e != nil {
-			return e
-		}
-		e = r.Del(ctx, id).Err()
-		if e != nil {
-			return e
-		}
 		return err
 	}
 	idEn := fmt.Sprintf("id_en:%d", info.IDEn)
 	err = r.Set(ctx, idEn, info.SystemObjectID, 0).Err()
 	if err != nil {
-		e := r.Del(ctx, info.SystemObjectID).Err()
-		if e != nil {
-			return e
-		}
-		e = r.Del(ctx, globalId).Err()
-		if e != nil {
-			return e
-		}
-		e = r.Del(ctx, id).Err()
-		if e != nil {
-			return e
-		}
-		e = r.Del(ctx, idEn).Err()
-		if e != nil {
-			return e
-		}
 		return err
 	}
 	mode := fmt.Sprintf("mode:%s", info.Mode)
 	err = r.RPush(ctx, mode, info.SystemObjectID).Err()
 	if err != nil {
-		e := r.Del(ctx, info.SystemObjectID).Err()
-		if e != nil {
-			return e
-		}
-		e = r.Del(ctx, globalId).Err()
-		if e != nil {
-			return e
-		}
-		e = r.Del(ctx, id).Err()
-		if e != nil {
-			return e
-		}
-		e = r.Del(ctx, idEn).Err()
-		if e != nil {
-			return e
-		}
-		err = r.RPop(ctx, mode).Err()
-		if e != nil {
-			return e
-		}
 		return err
 	}
 	modeEn := fmt.Sprintf("mode_en:%s", info.ModeEn)
 	err = r.RPush(ctx, modeEn, info.SystemObjectID).Err()
 	if err != nil {
-		e := r.Del(ctx, info.SystemObjectID).Err()
-		if e != nil {
-			return e
-		}
-		e = r.Del(ctx, globalId).Err()
-		if e != nil {
-			return e
-		}
-		e = r.Del(ctx, id).Err()
-		if e != nil {
-			return e
-		}
-		e = r.Del(ctx, idEn).Err()
-		if e != nil {
-			return e
-		}
-		e = r.RPop(ctx, mode).Err()
-		if e != nil {
-			return e
-		}
-		e = r.RPop(ctx, modeEn).Err()
-		if e != nil {
-			return e
-		}
 		return err
 	}
 	return nil
