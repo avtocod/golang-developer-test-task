2a16a43ae4d611d5cfee5c16cb3e4fad1e050872
Denis Skripov <nizhikebinesi@gmail.com>
2022-08-22 03:45:24 +0600

    add tests for FindValues single key

--- infrastructure/redclient/redis_test.go
+++ infrastructure/redclient/redis_test.go
@@ -73,3 +73,73 @@ func TestFindValuesNotFoundMultiple(t *testing.T) {
 		t.Fatal(err)
 	}
 }
+
+func TestFindValuesSingle(t *testing.T) {
+	info := structs.Info{
+		GlobalID:       42,
+		SystemObjectID: "777",
+		ID:             1,
+		IDEn:           9,
+		Mode:           "abc",
+		ModeEn:         "cba",
+	}
+
+	globalID := fmt.Sprintf("global_id:%d", info.GlobalID)
+	id := fmt.Sprintf("id:%d", info.ID)
+	idEn := fmt.Sprintf("id_en:%d", info.IDEn)
+	mode := fmt.Sprintf("mode:%s", info.Mode)
+	modeEn := fmt.Sprintf("mode_en:%s", info.ModeEn)
+
+	bs, _ := easyjson.Marshal(info)
+
+	db, mock := redismock.NewClientMock()
+	mock.ExpectWatch(info.SystemObjectID, globalID, id, idEn, mode, modeEn)
+	mock.ExpectGet(info.SystemObjectID).SetVal("")
+	mock.ExpectTxPipeline()
+	mock.ExpectSet(info.SystemObjectID, bs, 0).SetVal("OK")
+	mock.ExpectSet(fmt.Sprintf("global_id:%d", info.GlobalID), info.SystemObjectID, 0).SetVal("OK")
+	mock.ExpectSet(fmt.Sprintf("id:%d", info.ID), info.SystemObjectID, 0).SetVal("OK")
+	mock.ExpectSet(fmt.Sprintf("id_en:%d", info.IDEn), info.SystemObjectID, 0).SetVal("OK")
+	mock.ExpectRPush(fmt.Sprintf("mode:%s", info.Mode), info.SystemObjectID).SetVal(0)
+	mock.ExpectRPush(fmt.Sprintf("mode_en:%s", info.ModeEn), info.SystemObjectID).SetVal(0)
+	mock.ExpectTxPipelineExec()
+
+	key := info.SystemObjectID
+	mock.ExpectGet(key).SetVal(string(bs))
+	client := &RedisClient{*db}
+
+	client.AddValue(context.Background(), info)
+	infoList, totalSize, err := client.FindValues(context.Background(), key, false, 0, 0)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(infoList) != 1 {
+		t.Errorf("infoList length is not equal to 1; infoList: %v", infoList)
+	}
+	if size := len(infoList); int64(size) != totalSize {
+		t.Errorf("infoList length is not equal to totalSize; len(infoList) = %d ; totalSize = %d", size, totalSize)
+	}
+	if info != infoList[0] {
+		t.Errorf("data inside infoList is not info; info: %v ; infoList: %v", info, infoList)
+	}
+}
+
+func TestFindValuesSingleNothing(t *testing.T) {
+	db, mock := redismock.NewClientMock()
+	key := "777"
+	mock.ExpectGet(key).SetErr(redis.Nil)
+	client := &RedisClient{*db}
+
+	infoList, totalSize, err := client.FindValues(context.Background(), key, false, 0, 0)
+
+	if err != redis.Nil {
+		t.Fatal(err)
+	}
+	if len(infoList) != 0 {
+		t.Errorf("infoList length is not equal to 1; infoList: %v", infoList)
+	}
+	if size := len(infoList); int64(size) != totalSize {
+		t.Errorf("infoList length is not equal to totalSize; len(infoList) = %d ; totalSize = %d", size, totalSize)
+	}
+}
