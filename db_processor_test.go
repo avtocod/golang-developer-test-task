d755988ffd42031de6d8aff47ebb5aa593927ce9
Denis Skripov <nizhikebinesi@gmail.com>
2022-08-22 06:53:35 +0600

    add more tests for search handler

--- db_processor_test.go
+++ db_processor_test.go
@@ -167,6 +167,422 @@ func TestHandleSearchMode(t *testing.T) {
 	}
 }
 
+func TestHandleSearchModeEn(t *testing.T) {
+	info := structs.Info{
+		GlobalID:       42,
+		SystemObjectID: "777",
+		ID:             1,
+		IDEn:           9,
+		Mode:           "abc",
+		ModeEn:         "cba",
+	}
+
+	globalID := fmt.Sprintf("global_id:%d", info.GlobalID)
+	id := fmt.Sprintf("id:%d", info.ID)
+	idEn := fmt.Sprintf("id_en:%d", info.IDEn)
+	mode := fmt.Sprintf("mode:%s", info.Mode)
+	modeEn := fmt.Sprintf("mode_en:%s", info.ModeEn)
+
+	bs, _ := easyjson.Marshal(info)
+
+	db, mock := redismock.NewClientMock()
+	mock.ExpectWatch(info.SystemObjectID, globalID, id, idEn, mode, modeEn)
+	mock.ExpectGet(info.SystemObjectID).SetVal("")
+	mock.ExpectTxPipeline()
+	mock.ExpectSet(info.SystemObjectID, bs, 0).SetVal("OK")
+	mock.ExpectSet(globalID, info.SystemObjectID, 0).SetVal("OK")
+	mock.ExpectSet(id, info.SystemObjectID, 0).SetVal("OK")
+	mock.ExpectSet(idEn, info.SystemObjectID, 0).SetVal("OK")
+	mock.ExpectRPush(mode, info.SystemObjectID).SetVal(0)
+	mock.ExpectRPush(modeEn, info.SystemObjectID).SetVal(0)
+	mock.ExpectTxPipelineExec()
+
+	var paginationSize int64 = 5
+	mock.ExpectLLen(modeEn).SetVal(1)
+	mock.ExpectLRange(modeEn, 0, paginationSize).SetVal([]string{info.SystemObjectID})
+	mock.ExpectGet(info.SystemObjectID).SetVal(string(bs))
+
+	client := &redclient.RedisClient{*db}
+	err := client.AddValue(context.Background(), info)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	logger, _ := zap.NewProduction()
+	defer func() {
+		_ = logger.Sync()
+	}()
+
+	processor := NewDBProcessor(client, logger)
+
+	searchObject := structs.SearchObject{ModeEn: &info.ModeEn}
+	bs1, _ := easyjson.Marshal(searchObject)
+
+	req := httptest.NewRequest("POST", "/api/search", bytes.NewBuffer(bs1))
+	req.Header.Add("Content-Type", "application/json")
+	res := httptest.NewRecorder()
+	// processor.HandleLoadFile(res, req)
+	h := processor.MethodMiddleware(processor.HandleSearch, "POST")
+	h(res, req)
+
+	if res.Code != http.StatusOK {
+		t.Errorf("got status %d but wanted %d", res.Code, http.StatusOK)
+	}
+
+	var result structs.PaginationObject
+	err = easyjson.Unmarshal(res.Body.Bytes(), &result)
+	fmt.Println(err)
+	fmt.Println(result)
+
+	if result.Size != 1 {
+		t.Errorf("result.Size != 1; result = %v", result)
+	}
+	if len(result.Data) != 1 {
+		t.Errorf("len(result.Data) != 1; result = %v", result)
+	}
+	if result.HasNext {
+		t.Errorf("result has next page")
+	}
+	if result.HasPrevious {
+		t.Errorf("result has previous page")
+	}
+	if result.Data[0] != info {
+		t.Errorf("result data is not equal to expected; result data: %v ; expected: %v",
+			result.Data[0], info)
+	}
+}
+
+func TestHandleSearchID(t *testing.T) {
+	info := structs.Info{
+		GlobalID:       42,
+		SystemObjectID: "777",
+		ID:             1,
+		IDEn:           9,
+		Mode:           "abc",
+		ModeEn:         "cba",
+	}
+
+	globalID := fmt.Sprintf("global_id:%d", info.GlobalID)
+	id := fmt.Sprintf("id:%d", info.ID)
+	idEn := fmt.Sprintf("id_en:%d", info.IDEn)
+	mode := fmt.Sprintf("mode:%s", info.Mode)
+	modeEn := fmt.Sprintf("mode_en:%s", info.ModeEn)
+
+	bs, _ := easyjson.Marshal(info)
+
+	db, mock := redismock.NewClientMock()
+	mock.ExpectWatch(info.SystemObjectID, globalID, id, idEn, mode, modeEn)
+	mock.ExpectGet(info.SystemObjectID).SetVal("")
+	mock.ExpectTxPipeline()
+	mock.ExpectSet(info.SystemObjectID, bs, 0).SetVal("OK")
+	mock.ExpectSet(globalID, info.SystemObjectID, 0).SetVal("OK")
+	mock.ExpectSet(id, info.SystemObjectID, 0).SetVal("OK")
+	mock.ExpectSet(idEn, info.SystemObjectID, 0).SetVal("OK")
+	mock.ExpectRPush(mode, info.SystemObjectID).SetVal(0)
+	mock.ExpectRPush(modeEn, info.SystemObjectID).SetVal(0)
+	mock.ExpectTxPipelineExec()
+
+	mock.ExpectGet(id).SetVal(info.SystemObjectID)
+	mock.ExpectGet(info.SystemObjectID).SetVal(string(bs))
+
+	client := &redclient.RedisClient{*db}
+	err := client.AddValue(context.Background(), info)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	logger, _ := zap.NewProduction()
+	defer func() {
+		_ = logger.Sync()
+	}()
+
+	processor := NewDBProcessor(client, logger)
+
+	searchObject := structs.SearchObject{ID: &info.ID}
+	bs1, _ := easyjson.Marshal(searchObject)
+
+	req := httptest.NewRequest("POST", "/api/search", bytes.NewBuffer(bs1))
+	req.Header.Add("Content-Type", "application/json")
+	res := httptest.NewRecorder()
+	// processor.HandleLoadFile(res, req)
+	h := processor.MethodMiddleware(processor.HandleSearch, "POST")
+	h(res, req)
+
+	if res.Code != http.StatusOK {
+		t.Errorf("got status %d but wanted %d", res.Code, http.StatusOK)
+	}
+
+	var result structs.PaginationObject
+	err = easyjson.Unmarshal(res.Body.Bytes(), &result)
+	fmt.Println(err)
+	fmt.Println(result)
+
+	if result.Size != 1 {
+		t.Errorf("result.Size != 1; result = %v", result)
+	}
+	if len(result.Data) != 1 {
+		t.Errorf("len(result.Data) != 1; result = %v", result)
+	}
+	if result.HasNext {
+		t.Errorf("result has next page")
+	}
+	if result.HasPrevious {
+		t.Errorf("result has previous page")
+	}
+	if result.Data[0] != info {
+		t.Errorf("result data is not equal to expected; result data: %v ; expected: %v",
+			result.Data[0], info)
+	}
+}
+
+func TestHandleSearchIDEn(t *testing.T) {
+	info := structs.Info{
+		GlobalID:       42,
+		SystemObjectID: "777",
+		ID:             1,
+		IDEn:           9,
+		Mode:           "abc",
+		ModeEn:         "cba",
+	}
+
+	globalID := fmt.Sprintf("global_id:%d", info.GlobalID)
+	id := fmt.Sprintf("id:%d", info.ID)
+	idEn := fmt.Sprintf("id_en:%d", info.IDEn)
+	mode := fmt.Sprintf("mode:%s", info.Mode)
+	modeEn := fmt.Sprintf("mode_en:%s", info.ModeEn)
+
+	bs, _ := easyjson.Marshal(info)
+
+	db, mock := redismock.NewClientMock()
+	mock.ExpectWatch(info.SystemObjectID, globalID, id, idEn, mode, modeEn)
+	mock.ExpectGet(info.SystemObjectID).SetVal("")
+	mock.ExpectTxPipeline()
+	mock.ExpectSet(info.SystemObjectID, bs, 0).SetVal("OK")
+	mock.ExpectSet(globalID, info.SystemObjectID, 0).SetVal("OK")
+	mock.ExpectSet(id, info.SystemObjectID, 0).SetVal("OK")
+	mock.ExpectSet(idEn, info.SystemObjectID, 0).SetVal("OK")
+	mock.ExpectRPush(mode, info.SystemObjectID).SetVal(0)
+	mock.ExpectRPush(modeEn, info.SystemObjectID).SetVal(0)
+	mock.ExpectTxPipelineExec()
+
+	mock.ExpectGet(idEn).SetVal(info.SystemObjectID)
+	mock.ExpectGet(info.SystemObjectID).SetVal(string(bs))
+
+	client := &redclient.RedisClient{*db}
+	err := client.AddValue(context.Background(), info)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	logger, _ := zap.NewProduction()
+	defer func() {
+		_ = logger.Sync()
+	}()
+
+	processor := NewDBProcessor(client, logger)
+
+	searchObject := structs.SearchObject{IDEn: &info.IDEn}
+	bs1, _ := easyjson.Marshal(searchObject)
+
+	req := httptest.NewRequest("POST", "/api/search", bytes.NewBuffer(bs1))
+	req.Header.Add("Content-Type", "application/json")
+	res := httptest.NewRecorder()
+	// processor.HandleLoadFile(res, req)
+	h := processor.MethodMiddleware(processor.HandleSearch, "POST")
+	h(res, req)
+
+	if res.Code != http.StatusOK {
+		t.Errorf("got status %d but wanted %d", res.Code, http.StatusOK)
+	}
+
+	var result structs.PaginationObject
+	err = easyjson.Unmarshal(res.Body.Bytes(), &result)
+	fmt.Println(err)
+	fmt.Println(result)
+
+	if result.Size != 1 {
+		t.Errorf("result.Size != 1; result = %v", result)
+	}
+	if len(result.Data) != 1 {
+		t.Errorf("len(result.Data) != 1; result = %v", result)
+	}
+	if result.HasNext {
+		t.Errorf("result has next page")
+	}
+	if result.HasPrevious {
+		t.Errorf("result has previous page")
+	}
+	if result.Data[0] != info {
+		t.Errorf("result data is not equal to expected; result data: %v ; expected: %v",
+			result.Data[0], info)
+	}
+}
+
+func TestHandleSearchSystemObjectID(t *testing.T) {
+	info := structs.Info{
+		GlobalID:       42,
+		SystemObjectID: "777",
+		ID:             1,
+		IDEn:           9,
+		Mode:           "abc",
+		ModeEn:         "cba",
+	}
+
+	globalID := fmt.Sprintf("global_id:%d", info.GlobalID)
+	id := fmt.Sprintf("id:%d", info.ID)
+	idEn := fmt.Sprintf("id_en:%d", info.IDEn)
+	mode := fmt.Sprintf("mode:%s", info.Mode)
+	modeEn := fmt.Sprintf("mode_en:%s", info.ModeEn)
+
+	bs, _ := easyjson.Marshal(info)
+
+	db, mock := redismock.NewClientMock()
+	mock.ExpectWatch(info.SystemObjectID, globalID, id, idEn, mode, modeEn)
+	mock.ExpectGet(info.SystemObjectID).SetVal("")
+	mock.ExpectTxPipeline()
+	mock.ExpectSet(info.SystemObjectID, bs, 0).SetVal("OK")
+	mock.ExpectSet(globalID, info.SystemObjectID, 0).SetVal("OK")
+	mock.ExpectSet(id, info.SystemObjectID, 0).SetVal("OK")
+	mock.ExpectSet(idEn, info.SystemObjectID, 0).SetVal("OK")
+	mock.ExpectRPush(mode, info.SystemObjectID).SetVal(0)
+	mock.ExpectRPush(modeEn, info.SystemObjectID).SetVal(0)
+	mock.ExpectTxPipelineExec()
+
+	mock.ExpectGet(info.SystemObjectID).SetVal(string(bs))
+
+	client := &redclient.RedisClient{*db}
+	err := client.AddValue(context.Background(), info)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	logger, _ := zap.NewProduction()
+	defer func() {
+		_ = logger.Sync()
+	}()
+
+	processor := NewDBProcessor(client, logger)
+
+	searchObject := structs.SearchObject{SystemObjectID: &info.SystemObjectID}
+	bs1, _ := easyjson.Marshal(searchObject)
+
+	req := httptest.NewRequest("POST", "/api/search", bytes.NewBuffer(bs1))
+	req.Header.Add("Content-Type", "application/json")
+	res := httptest.NewRecorder()
+	// processor.HandleLoadFile(res, req)
+	h := processor.MethodMiddleware(processor.HandleSearch, "POST")
+	h(res, req)
+
+	if res.Code != http.StatusOK {
+		t.Errorf("got status %d but wanted %d", res.Code, http.StatusOK)
+	}
+
+	var result structs.PaginationObject
+	err = easyjson.Unmarshal(res.Body.Bytes(), &result)
+	fmt.Println(err)
+	fmt.Println(result)
+
+	if result.Size != 1 {
+		t.Errorf("result.Size != 1; result = %v", result)
+	}
+	if len(result.Data) != 1 {
+		t.Errorf("len(result.Data) != 1; result = %v", result)
+	}
+	if result.HasNext {
+		t.Errorf("result has next page")
+	}
+	if result.HasPrevious {
+		t.Errorf("result has previous page")
+	}
+	if result.Data[0] != info {
+		t.Errorf("result data is not equal to expected; result data: %v ; expected: %v",
+			result.Data[0], info)
+	}
+}
+
+func TestHandleSearchGlobalID(t *testing.T) {
+	info := structs.Info{
+		GlobalID:       42,
+		SystemObjectID: "777",
+		ID:             1,
+		IDEn:           9,
+		Mode:           "abc",
+		ModeEn:         "cba",
+	}
+
+	globalID := fmt.Sprintf("global_id:%d", info.GlobalID)
+	id := fmt.Sprintf("id:%d", info.ID)
+	idEn := fmt.Sprintf("id_en:%d", info.IDEn)
+	mode := fmt.Sprintf("mode:%s", info.Mode)
+	modeEn := fmt.Sprintf("mode_en:%s", info.ModeEn)
+
+	bs, _ := easyjson.Marshal(info)
+
+	db, mock := redismock.NewClientMock()
+	mock.ExpectWatch(info.SystemObjectID, globalID, id, idEn, mode, modeEn)
+	mock.ExpectGet(info.SystemObjectID).SetVal("")
+	mock.ExpectTxPipeline()
+	mock.ExpectSet(info.SystemObjectID, bs, 0).SetVal("OK")
+	mock.ExpectSet(globalID, info.SystemObjectID, 0).SetVal("OK")
+	mock.ExpectSet(id, info.SystemObjectID, 0).SetVal("OK")
+	mock.ExpectSet(idEn, info.SystemObjectID, 0).SetVal("OK")
+	mock.ExpectRPush(mode, info.SystemObjectID).SetVal(0)
+	mock.ExpectRPush(modeEn, info.SystemObjectID).SetVal(0)
+	mock.ExpectTxPipelineExec()
+
+	mock.ExpectGet(globalID).SetVal(info.SystemObjectID)
+	mock.ExpectGet(info.SystemObjectID).SetVal(string(bs))
+
+	client := &redclient.RedisClient{*db}
+	err := client.AddValue(context.Background(), info)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	logger, _ := zap.NewProduction()
+	defer func() {
+		_ = logger.Sync()
+	}()
+
+	processor := NewDBProcessor(client, logger)
+
+	searchObject := structs.SearchObject{GlobalID: &info.GlobalID}
+	bs1, _ := easyjson.Marshal(searchObject)
+
+	req := httptest.NewRequest("POST", "/api/search", bytes.NewBuffer(bs1))
+	req.Header.Add("Content-Type", "application/json")
+	res := httptest.NewRecorder()
+	// processor.HandleLoadFile(res, req)
+	h := processor.MethodMiddleware(processor.HandleSearch, "POST")
+	h(res, req)
+
+	if res.Code != http.StatusOK {
+		t.Errorf("got status %d but wanted %d", res.Code, http.StatusOK)
+	}
+
+	var result structs.PaginationObject
+	err = easyjson.Unmarshal(res.Body.Bytes(), &result)
+	fmt.Println(err)
+	fmt.Println(result)
+
+	if result.Size != 1 {
+		t.Errorf("result.Size != 1; result = %v", result)
+	}
+	if len(result.Data) != 1 {
+		t.Errorf("len(result.Data) != 1; result = %v", result)
+	}
+	if result.HasNext {
+		t.Errorf("result has next page")
+	}
+	if result.HasPrevious {
+		t.Errorf("result has previous page")
+	}
+	if result.Data[0] != info {
+		t.Errorf("result data is not equal to expected; result data: %v ; expected: %v",
+			result.Data[0], info)
+	}
+}
+
 func TestHandleLoadFromURLBadRequest(t *testing.T) {
 	db, _ := redismock.NewClientMock()
 	client := &redclient.RedisClient{*db}
